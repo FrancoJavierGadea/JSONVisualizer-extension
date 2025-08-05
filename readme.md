dbeb89749a9e445de763b0bcbf263e543516bf86
1812 <francogadea1812@gmail.com>
2025-08-05 15:21:05 -0300

    feat: add config files and integrate with Vite

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+#### npm run dev
+
+- [http://localhost:5173/src/popup/index.html](http://localhost:5173/src/popup/index.html)
+
+- [http://localhost:5173/src/view/index.html](http://localhost:5173/src/view/index.html)
+
+- [http://localhost:5173/src/config/index.html](http://localhost:5173/src/config/index.html)
+
--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,15 @@ export default defineConfig({
             input: {
                 popup: resolve(__dirname, 'src/popup/index.html'),
                 view: resolve(__dirname, 'src/view/index.html'),
+                config: resolve(__dirname, 'src/config/index.html'),
                 background: resolve(__dirname, 'src/background.js'),
                 content: resolve(__dirname, 'src/content.js')
             },
             output: {
                 entryFileNames: (chunk) => {
-                if (chunk.name === 'background') return 'background.js';
-                if (chunk.name === 'content') return 'content.js';
-                return 'assets/[name].js';
+                    if (chunk.name === 'background') return 'background.js';
+                    if (chunk.name === 'content') return 'content.js';
+                    return 'assets/[name].js';
                 }
             }
         }
